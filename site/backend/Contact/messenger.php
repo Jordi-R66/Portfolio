afa06b596dfecdce010f60e4f77fd10bbdebc973
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-12-12 08:49:18 +0100

    Mise à jour du code messager Mise à jour du script sql de création de la base de données Changement de CV

--- site/backend/Contact/messenger.php
+++ site/backend/Contact/messenger.php
@@ -7,20 +7,59 @@ function transformerIP(string $ip): string {
 	return bin2hex(inet_pton($ip));
 }
 
-function ajouterMessage($ip, $sujet, $corps, $tel, $mail) {
+function checkIp(string $ip): bool {
+	$output = false;
+
+	$msg_time = new DateTime();
+	$msg_ts = $msg_time->getTimestamp();
+
 	$pdo = Database::getPDO();
-	$sql = "INSERT INTO historiqueContact (ipMessage,sujetMessage,corpsMessage,telephone,email)
-	VALUES (:ip, :sujet, :corps, :tel, :mail);";
+	$sql = "SELECT timestampMessage FROM historiqueContact WHERE ipMessage = :ip ORDER BY timestampMessage DESC LIMIT 1;";
 
 	$stmt = $pdo->prepare($sql);
 
 	$stmt->execute([
-		"ip" => transformerIP($ip),
-		"sujet" => $sujet,
-		"corps" => $corps,
-		"tel" => $tel,
-		"mail" => $mail
+		"ip" => transformerIP($ip)
 	]);
+
+	$row = $stmt->fetch();
+
+	var_dump($row);
+
+	if ($row != false) {
+		$ts = intval($row[0]);
+
+		if (($msg_ts - $ts) >= (5 * 60)) {
+			$output = true;
+		}
+	} else {
+		$output = true;
+	}
+
+	return $output;
+}
+
+function ajouterMessage($ip, $sujet, $corps, $tel, $mail) {
+	$msg_time = new DateTime();
+	$msg_ts = $msg_time->getTimestamp();
+	$canSend = checkIp($ip);
+
+	if ($canSend) {
+		$pdo = Database::getPDO();
+		$sql = "INSERT INTO historiqueContact (ipMessage,timestampMessage,sujetMessage,corpsMessage,telephone,email)
+		VALUES (:ip, :msgTS, :sujet, :corps, :tel, :mail);";
+
+		$stmt = $pdo->prepare($sql);
+
+		$stmt->execute([
+			"ip" => transformerIP($ip),
+			"msgTS" => $msg_ts,
+			"sujet" => $sujet,
+			"corps" => $corps,
+			"tel" => $tel,
+			"mail" => $mail
+		]);
+	}
 }
 
 ?>
