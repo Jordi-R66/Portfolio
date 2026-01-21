493662b3d759355668eda783d1dfbba8ac499d69
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-01-21 15:03:13 +0100

    Correction

--- site/projets.php
+++ site/projets.php
@@ -20,7 +20,7 @@
 				<hr>
 				<table class="langages-projet">
 					<tr>
-						<th>Langage</th>
+						<th>Technologie</th>
 						<th>Utilisation</th>
 					</tr>
 					<tr>
@@ -41,7 +41,7 @@
 
 				<p>À la fin de la Terminale, en 2024, j'ai donc commencé à travailler sur ce projet. Je me base sur les lois de Kepler pour ensuite déterminer l'emplacement de n'importe quel satellite en orbite terrestre. À l'origine comme le nom l'indique c'était dédié au parsing des TLE (Two Line Elements), à savoir le format dans lequel sont encodées toutes les données nécessaires à tout algorithme ayant pour but de calculer l'emplacement d'un satellite. Ensuite, je me suis dit que quittes à avoir un moyen d'obtenir et décoder ces données, autant me servir de ces données, j'ai donc fini par coder ce projet</p>
 
-				<p>Je suis à la fois fier et honteux de ce projet, en effet les matrices de transformations pour passer de coordonnées 2D vers 3D sont incorrectes, je me retrouve souvent avec des coordonnées qui indiquent une position du satellite au centre de la Terre et quelques secondes plus tard à l'apogée de l'orbite voire plus loin. Aussi, le code source contient beaucoup de vestiges de l'époque où je ne connaissais que trop mal le C. Ce sont donc mes deux raisons d'avoir un peu honte de ce projet</p>
+				<p>Je suis à la fois fier et honteux de ce projet, en effet les matrices de transformations pour passer de coordonnées 2D vers 3D sont incorrectes, je me retrouve souvent avec des coordonnées qui indiquent une position du satellite au centre de la Terre et quelques secondes plus tard à l'apogée de l'orbite voire plus loin. Aussi, le code source contient beaucoup de vestiges de l'époque où je ne connaissais que trop mal le C. Ce sont donc mes deux raisons d'avoir un peu honte de ce projet.</p>
 			</div>
 		</div>
 		<hr>
@@ -52,7 +52,7 @@
 				<hr>
 				<table class="langages-projet">
 					<tr>
-						<th>Langage</th>
+						<th>Technologie</th>
 						<th>Utilisation</th>
 					</tr>
 					<tr>
@@ -78,7 +78,7 @@
 				<hr>
 				<table class="langages-projet">
 					<tr>
-						<th>Langage</th>
+						<th>Technologie</th>
 						<th>Utilisation</th>
 					</tr>
 					<tr>
