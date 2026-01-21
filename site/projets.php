c1942e1cc5b4622b074e63940affb4cfb62ec5de
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-01-21 13:56:08 +0100

    Ajout des détails pour la SAÉ Enchères

--- site/projets.php
+++ site/projets.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<?php require_once "backend/html/head.html"; ?>
 	<meta name="robots" content="noindex">
@@ -7,6 +8,7 @@
 
 	<title>Jordi Rocafort - Projets</title>
 </head>
+
 <body>
 	<?php require_once "backend/html/header.html"; ?>
 
@@ -39,7 +41,7 @@
 
 				<p>À la fin de la Terminale, en 2024, j'ai donc commencé à travailler sur ce projet. Je me base sur les lois de Kepler pour ensuite déterminer l'emplacement de n'importe quel satellite en orbite terrestre. À l'origine comme le nom l'indique c'était dédié au parsing des TLE (Two Line Elements), à savoir le format dans lequel sont encodées toutes les données nécessaires à tout algorithme ayant pour but de calculer l'emplacement d'un satellite. Ensuite, je me suis dit que quittes à avoir un moyen d'obtenir et décoder ces données, autant me servir de ces données, j'ai donc fini par coder ce projet</p>
 
-				<p>Je suis à la fois fier et honteux de ce projet, (j'ai tort d'en avoir honte j'en suis conscient), en effet les matrices de transformations pour passer de coordonnées 2D vers 3D sont incorrectes, je me retrouve souvent avec des coordonnées qui indiquent une position du satellite au centre de la Terre et quelques secondes plus tard à l'apogée de l'orbite voire plus loin. Aussi, le code source contient beaucoup de vestigent de l'époque où je ne connaissais que trop mal le C. Ce sont donc mes deux raisons d'avoir un peu honte de ce projet</p>
+				<p>Je suis à la fois fier et honteux de ce projet, en effet les matrices de transformations pour passer de coordonnées 2D vers 3D sont incorrectes, je me retrouve souvent avec des coordonnées qui indiquent une position du satellite au centre de la Terre et quelques secondes plus tard à l'apogée de l'orbite voire plus loin. Aussi, le code source contient beaucoup de vestiges de l'époque où je ne connaissais que trop mal le C. Ce sont donc mes deux raisons d'avoir un peu honte de ce projet</p>
 			</div>
 		</div>
 		<hr>
@@ -58,6 +60,14 @@
 						<td>Entièreté</td>
 					</tr>
 				</table>
+			</div>
+
+			<img class="illus-projet" src="img/Screenshot-Javenchère.png" alt="Vous pouvez voir sur l'interface des comptes prédéfinis, il s'agit là d'un choix pratique pour les démonstrations">
+
+			<div class="description-projet">
+				<p>Dans le cadre de la SAÉ du troisème semestre de mon BUT Informatique, j'ai participé au développement d'un système d'enchères sécurisées à plis fermés (Lors des enchères personne ne connaît les prix que les acheteurs proposent, et sont révélés à la fin de la phase d'enchère). Pour être plus précis, il s'agit des enchères de Vickrey, une forme d'enchères reposant sur la théorie des jeux. Dans ce modèle, celui qui a proposé le plus haut prix remporte l'enchère mais ne paye que le deuxième plus haut prix.</p>
+
+				<p>Au cours de ce projet je me suis principalement chargé de l'élaboration de la communication entre le logiciel utilisateur et la partie serveur. J'ai écrit tout le code nécessaire à communication entre les utilisateurs et le serveur et assuré le chiffrement en utilisant SSL/TLS via un certificat auto-signé, et l'emploi de RSA 4096. L'interface graphique ainsi que la logique de la gestion des enchères ont été réalisées par les autres membres de l'équipe.</p>
 			</div>
 		</div>
 		<hr>
@@ -82,4 +92,5 @@
 
 	<?php require_once "backend/html/footer.html"; ?>
 </body>
+
 </html>
