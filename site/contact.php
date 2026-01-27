f121889c9fe26ac9bad869c17bc89bc83cb2922f
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-01-27 17:12:14 +0100

    Test validé adaptation de contact.php

--- site/contact.php
+++ site/contact.php
@@ -1,82 +1,22 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-	<?php require_once "backend/html/head.html"; ?>
-	<meta name="robots" content="noindex">
-	<link href="css/contact.css" rel="stylesheet" type="text/css">
+require_once __DIR__ . '/backend/pageloader/PageLoader.php';
 
-	<title>Jordi Rocafort - Contact</title>
-</head>
+$content = PageLoader::loadHTML("contact");
 
-<body>
-	<?php require_once "backend/html/header.html"; ?>
+$status = "";
 
-	<main>
-		<div id="info-contact-box">
-			<h3>Comment me contacter</h3>
+if (isset($_GET["statut"]) && ($_GET["statut"] === "0" || $_GET["statut"] === "1")) {
+	$sent = intval($_GET["statut"]) == 1;
 
-			<p>Suite à des abus commis sur mon adresse mail, j'ai décidé de la retirer du portfolio, merci de me contacter soit par LinkedIn/GitHub soit par le formulaire ci-dessous.</p>
+	$color = $sent == true ? "green" : "red";
+	$texte = $sent == true ? "Votre message a pu être envoyé !" : "Votre message n'a pas pu être envoyé, veuillez réessayer plus tard";
 
-			<?php
-			if (isset($_GET["statut"]) && ($_GET["statut"] === "0" || $_GET["statut"] === "1")) {
-				$sent = intval($_GET["statut"]) == 1;
+	$status = "<h4 style=\"color: $color;\">$texte</h4>";
+}
 
-				$color = $sent == true ? "green" : "red";
-				$texte = $sent == true ? "Votre message a pu être envoyé !" : "Votre message n'a pas pu être envoyé, veuillez réessayer plus tard";
+$content = str_replace("MESSAGE_STATUS", $status, $content);
 
-				echo "<h4 style=\"color: $color;\">$texte</h4>";
-			}
-			?>
-
-			<div id="info-contact-content">
-
-				<div id="info-contact-leftbox">
-					<h3>Retrouvez-moi</h3>
-					<p>Envie d'en savoir plus sur mon parcours ou de voir mes codes sources ? Connectons-nous !</p>
-
-					<ul class="contact-links">
-						<li>
-							<a class="social-btn linkedin" href="https://linkedin.com/in/jordi-rocafort" target="_blank">
-								<img class="socials-icon" src="img/linkedin_icon.svg" alt="LinkedIn Logo">
-								<span>LinkedIn</span>
-							</a>
-						</li>
-						<li>
-							<a class="social-btn github" href="https://github.com/Jordi-R66/" target="_blank">
-								<img class="socials-icon" src="img/github_icon.svg" alt="GitHub Logo">
-								<span>GitHub</span>
-							</a>
-						</li>
-					</ul>
-				</div>
-
-				<div id="info-contact-formulaire">
-					<form action="facadeMessager.php" method="POST">
-
-						<label for="objet">Objet</label>
-						<input id="objet" name="objet" type="text" required>
-
-						<label for="corps">Message</label>
-						<textarea id="corps" name="corps" required></textarea>
-
-						<label for="telephone">Téléphone</label>
-						<input id="telephone" name="telephone" type="tel"
-							pattern="\+?(\d{1,3})?[\s.-]?(?:\(?\d{1,4}\)?[\s.-]?)*\d{1,4}">
-
-						<label for="email">Email</label>
-						<input id="email" name="email" type="email">
-
-						<button type="submit">Envoyer</button>
-					</form>
-				</div>
-
-			</div>
-
-		</div>
-	</main>
-
-	<?php require_once "backend/html/footer.html"; ?>
-</body>
-
-</html>
+echo $content;
+exit;
+?>
